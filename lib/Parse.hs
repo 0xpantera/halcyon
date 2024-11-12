e22273ea4781782397d7ee90d74dc09b07fa7de0
0xpantera <0xpantera@proton.me>
2024-11-12 12:50:07 +0100

    Improve parser to be more idiomatic and consistent with Lexer

--- lib/Parse.hs
+++ lib/Parse.hs
@@ -1,79 +1,98 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Parse where
+module Parse 
+  ( parseTokens
+  , HalcyonParseError
+  , ParseResult
+  ) where
 
 import Control.Monad (void)
-import Data.Void
 import Data.Text (Text)
+import qualified Data.Set as Set
+import Data.List.NonEmpty (NonEmpty(..))
+import qualified Data.List.NonEmpty as NE
 import Text.Megaparsec
+import Control.Monad.Combinators.Expr
 
 import Tokens
 import Ast
 
--- Specialized parser type for working with tokens
-type Parser = Parsec Void [CToken]
+-- Type aliases to make signatures cleaner
+type Parser = Parsec CTokenParseError [CToken]
+type HalcyonParseError = ParseErrorBundle [CToken] CTokenParseError
+type ParseResult = Either HalcyonParseError Program
 
--- Custom token parser
-tokenParser :: (CToken -> Maybe a) -> Parser a
-tokenParser test = token' where
-  token' = do
-    cTokens <- getInput
-    case cTokens of
-      [] -> fail "unexpected end of input"
-      (t:ts) -> case test t of
-        Nothing -> fail $ "unexpected token: " ++ show t
-        Just x -> do
-          setInput ts
-          return x
+-- Main entry point
+parseTokens :: [CToken] -> ParseResult
+parseTokens ctokens = runParser (parseProgram <* eof) "" ctokens
 
--- Match a specific token
+-- Program level parser
+parseProgram :: Parser Program
+parseProgram = Program <$> parseFunctionDef
+
+-- Function definition parsers
+parseFunctionDef :: Parser FunctionDef
+parseFunctionDef = 
+  Function <$> parseFunctionHeader <*> parseFunctionBody
+
+parseFunctionHeader :: Parser Text
+parseFunctionHeader = do
+  void $ matchToken TokInt
+  name <- identifier
+  void $ parseParams
+  pure name
+
+parseParams :: Parser ()
+parseParams = void $ between 
+  (matchToken TokLParen) 
+  (matchToken TokRParen)
+  (matchToken TokVoid)
+
+parseFunctionBody :: Parser Statement
+parseFunctionBody = between 
+  (matchToken TokLBrace)
+  (matchToken TokRBrace)
+  parseStatement
+
+-- Statement parsers
+parseStatement :: Parser Statement
+parseStatement = choice
+  [ parseReturn
+  ] <?> "statement"
+
+parseReturn :: Parser Statement
+parseReturn = Return <$> 
+  (matchToken TokReturn *> parseExpr <* matchToken TokSemicolon)
+  <?> "return statement"
+
+-- Expression parsers
+-- Currently only handles constants, but structured for future expansion
+parseExpr :: Parser Expr
+parseExpr = parseTerm <?> "expression"
+
+parseTerm :: Parser Expr
+parseTerm = choice
+  [ Constant <$> number
+  ] <?> "term"
+
+-- Token level parsers
 matchToken :: CToken -> Parser CToken
-matchToken t = tokenParser test
+matchToken expected = token test expectedSet
   where
-    test x = if x == t then Just x else Nothing
-
--- Parse an identifier token and extract the name
+    test x = if x == expected then Just x else Nothing
+    expectedSet = Set.singleton $ Tokens (expected :| [])
+    
 identifier :: Parser Text
-identifier = tokenParser test
+identifier = token test Set.empty <?> "identifier"
   where
     test (TokIdent t) = Just t
     test _ = Nothing
 
--- Parse a number token and extract the value
 number :: Parser Int
-number = tokenParser test
+number = token test Set.empty <?> "number"
   where
     test (TokNumber n) = Just n
     test _ = Nothing
 
--- Parse an expression (currently only constants)
-parseExpr :: Parser Expr
-parseExpr = Constant <$> number
-
--- Parse a return statement
-parseStatement :: Parser Statement
-parseStatement = do
-  void $ matchToken TokReturn
-  expr <- parseExpr
-  void $ matchToken TokSemicolon
-  return $ Return expr
-
--- Parse a function definition
-parseFunctionDef :: Parser FunctionDef
-parseFunctionDef = do
-  void $ matchToken TokInt
-  name <- identifier
-  void $ matchToken TokLParen
-  void $ matchToken TokVoid
-  void $ matchToken TokRParen
-  void $ matchToken TokLBrace
-  body <- parseStatement
-  void $ matchToken TokRBrace
-  return $ Function name body
-
--- Parse a complete program
-parseProgram :: Parser Program
-parseProgram = Program <$> parseFunctionDef
-
--- Main entry point
-parse :: [CToken] -> Either (ParseErrorBundle [CToken] Void) Program
-parse cTokens = runParser (parseProgram <* eof) "" cTokens
+-- Prepared for future operator implementation
+operatorTable :: [[Operator Parser Expr]]
+operatorTable = []
