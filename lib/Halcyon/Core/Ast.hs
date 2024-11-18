c2f5a967e3e81f8dda3fd9d8a9a538214fbe15e8
0xpantera <0xpantera@proton.me>
2024-11-18 15:28:14 +0100

    add unary operator variant to expression data type

--- lib/Halcyon/Core/Ast.hs
+++ lib/Halcyon/Core/Ast.hs
@@ -13,19 +13,24 @@ data FunctionDef = Function
 data Statement = Return Expr
   deriving Show
 
-data Expr = Constant Int
+data Expr = Constant Int | Unary UnaryOp Expr
+  deriving Show
+
+data UnaryOp = Complement | Negate
   deriving Show
 
 -- Formal Grammar in EBNF
 -- <program> ::= <function>
 -- <function> ::= "int" <identifier> "(" "void" ")" "{" <statement> "}"
 -- <statement> ::= "return" <exp> ";"
--- <exp> ::= <int>
+-- <exp> ::= <int> | <unop> <exp> | "(" <exp> ")"
+-- <unop> ::= "-" | "~"
 -- <identifier> ::= ? An identifier token ?
 -- <int> ::= ? A constant token ?
 
 -- AST in ASDL
---program = Program(function_definition)
---function_definition = Function(identifier name, statement body)
---statement = Return(exp)
---exp = Constant(int)
+-- program = Program(function_definition)
+-- function_definition = Function(identifier name, statement body)
+-- statement = Return(exp)
+-- exp = Constant(int) | Unary(unary_op, exp)
+-- unaryOp = Complement | Negate
