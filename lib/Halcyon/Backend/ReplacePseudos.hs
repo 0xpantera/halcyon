534803af8b4672adf1bf5eb9d79acaac46266be6
0xpantera <0xpantera@proton.me>
2024-11-22 10:39:05 +0100

    Add compiler pass to replace pseudoregisters, allocate stack and fix invalid mov instructions

--- lib/Halcyon/Backend/ReplacePseudos.hs
+++ lib/Halcyon/Backend/ReplacePseudos.hs
@@ -1,7 +1,14 @@
 {-# LANGUAGE RecordWildCards #-}
-module Halcyon.Backend.ReplacePseudos where
+{-# LANGUAGE OverloadedRecordDot #-}
+module Halcyon.Backend.ReplacePseudos 
+  ( replacePseudos
+  , fixupProgram
+  , ReplacePseudosError(..)
+  , PseudoM
+  ) where
 
-import Data.Traversable (mapAccumL)
+import Control.Monad.Except
+import Control.Monad.State
 import qualified Data.Map.Strict as Map
 import Data.Text ( Text )
 
@@ -13,45 +20,79 @@ data ReplacementSt = ReplacementSt
   , offsetMap     :: Map.Map Text Int -- map from pseudoregister to stack slots
   } deriving Show
 
-replaceOperand :: ReplacementSt -> Asm.Operand -> (ReplacementSt, Asm.Operand)
-replaceOperand state (Asm.Pseudo s) = case Map.lookup s (offsetMap state) of
-  Just offset -> (state, Asm.Stack offset)
-  Nothing -> 
-    let 
-      newOffset = (currentOffset state) - 4
-      newMap = Map.insert s newOffset (offsetMap state)
-      newState = ReplacementSt {
+data ReplacePseudosError 
+  = UnexpectedAllocateStack
+  deriving (Show, Eq)
+
+type PseudoM = Either ReplacePseudosError
+
+-- | State monad transformer that can also fail with ReplacePseudosError
+type ReplaceM = StateT ReplacementSt PseudoM
+
+-- | Constraint for monads that can perform compiler operations
+type MonadReplace m = 
+  ( MonadState ReplacementSt m
+  , MonadError ReplacePseudosError m
+  )
+
+replaceOperand :: MonadReplace m => Asm.Operand -> m Asm.Operand
+replaceOperand (Asm.Pseudo s) = do
+  st <- get
+  case Map.lookup s st.offsetMap of
+    Just offset -> pure $ Asm.Stack offset
+    Nothing -> do
+      let 
+        newOffset =  st.currentOffset - 4
+        newMap = Map.insert s newOffset st.offsetMap
+      put $ ReplacementSt {
         currentOffset = newOffset,
         offsetMap = newMap
       }
-    in (newState, Asm.Stack newOffset)
-replaceOperand state other = (state, other)
+      pure $ Asm.Stack newOffset
+replaceOperand other = pure other
 
-replacePseudoInst :: ReplacementSt -> Asm.Instruction -> (ReplacementSt, Asm.Instruction)
-replacePseudoInst state (Asm.Mov src dst) =
-  let 
-    (state1, newSrc) = replaceOperand state src
-    (state2, newDst) = replaceOperand state1 dst
-    newMov = Asm.Mov newSrc newDst
-    in (state2, newMov)
-replacePseudoInst state (Asm.Unary op dst) =
-  let
-    (state1, newDst) = replaceOperand state dst
-    newUnary = Asm.Unary op newDst
-  in (state1, newUnary)
-replacePseudoInst state Asm.Ret = (state, Asm.Ret)
-replacePseudoInst state (Asm.AllocateStack _) = (state, Asm.AllocateStack 900)
+replacePseudoInst :: MonadReplace m => Asm.Instruction -> m Asm.Instruction
+replacePseudoInst = \case
+  Asm.Mov src dst -> do
+    newSrc <- replaceOperand src
+    newDst <- replaceOperand dst
+    pure $ Asm.Mov newSrc newDst
+  Asm.Unary op dst -> do
+    newDst <- replaceOperand dst
+    pure $ Asm.Unary op newDst
+  Asm.Ret -> pure Asm.Ret
+  Asm.AllocateStack _ -> throwError UnexpectedAllocateStack
 
-replacePseudoFunc :: Asm.FunctionDef -> (ReplacementSt, Asm.FunctionDef)
-replacePseudoFunc func@Asm.Function{..} =
-  let 
-    initState = ReplacementSt { currentOffset = 0, offsetMap = Map.empty}
-    (finalState, fixedInstructions) = 
-      mapAccumL replacePseudoInst initState instructions
+replacePseudoFunc :: MonadReplace m => Asm.FunctionDef -> m Asm.FunctionDef
+replacePseudoFunc func@Asm.Function{..} = do
+  fixedInstructions <- traverse replacePseudoInst instructions
+  pure $ func { Asm.instructions = fixedInstructions }
 
-  in (finalState, func {Asm.instructions = fixedInstructions})
+replacePseudos :: Asm.Program -> PseudoM (Asm.Program, Int)
+replacePseudos (Asm.Program f) = do
+  let initState = ReplacementSt 
+        { currentOffset = 0
+        , offsetMap = Map.empty
+        }
+  -- Run the stateful computation and get both result and final state
+  (fixedF, finalState) <- runStateT (replacePseudoFunc f) initState
+  -- Return the program and just the offset we need
+  pure (Asm.Program fixedF, currentOffset finalState)
 
-replacePseudos :: Asm.Program -> (Int, Asm.Program)
-replacePseudos (Asm.Program f) =
-  let (lastState, fixedF) = replacePseudoFunc f
-  in (currentOffset lastState, Asm.Program fixedF)
+fixupInstruction :: Asm.Instruction -> [Asm.Instruction]
+fixupInstruction (Asm.Mov (Asm.Stack src) (Asm.Stack dst)) = 
+  [ (Asm.Mov (Asm.Stack src) (Asm.Register Asm.R10)) 
+  , (Asm.Mov (Asm.Register Asm.R10) (Asm.Stack dst)) ]
+fixupInstruction other = [ other ]
+
+fixupFunction :: Asm.FunctionDef -> Int ->Asm.FunctionDef
+fixupFunction (Asm.Function{..}) lastStackSlot = Asm.Function 
+  { name
+  , instructions = 
+      (Asm.AllocateStack lastStackSlot) 
+        : concatMap fixupInstruction instructions
+  }
+
+fixupProgram :: Asm.Program -> Int -> Asm.Program
+fixupProgram (Asm.Program f) lastStackSlot =
+  Asm.Program (fixupFunction f lastStackSlot) 
