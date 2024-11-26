089ecdb4e2691ea56d351a56f427332ea0d8f87b
0xpantera <0xpantera@proton.me>
2024-11-26 12:35:48 +0100

    add binary operations to types

--- lib/Halcyon/Core/Ast.hs
+++ lib/Halcyon/Core/Ast.hs
@@ -5,34 +5,47 @@ import Data.Text ( Text )
 data Program = Program Function
   deriving (Eq, Show)
 
-data Function = Function 
-  { name :: Text
-  , body :: Statement
-  } deriving (Eq, Show)
+data Function = Function Text Statement
+  deriving (Eq, Show)
 
 data Statement = Return Expr
   deriving (Eq, Show)
 
 data Expr
-  = Constant { value :: Int }
-  | Unary { operator :: UnaryOp, operand :: Expr }
+  = Constant Int
+  | Unary UnaryOp Expr
+  | Binary BinaryOp Expr Expr
   deriving (Eq, Show)
 
 data UnaryOp = Complement | Negate
   deriving (Eq, Show)
 
--- Formal Grammar in EBNF
--- <program> ::= <function>
--- <function> ::= "int" <identifier> "(" "void" ")" "{" <statement> "}"
--- <statement> ::= "return" <exp> ";"
--- <exp> ::= <int> | <unop> <exp> | "(" <exp> ")"
--- <unop> ::= "-" | "~"
--- <identifier> ::= ? An identifier token ?
--- <int> ::= ? A constant token ?
+data BinaryOp = Add | Subtract | Multiply | Divide | Remainder
+  deriving (Eq, Show)
+
 
 -- AST in ASDL
--- program = Program(function_definition)
--- function_definition = Function(identifier name, statement body)
--- statement = Return(exp)
--- exp = Constant(int) | Unary(unary_op, exp)
--- unaryOp = Complement | Negate
+{-
+program = Program(function_definition)
+function_definition = Function(identifier name, statement body)
+statement = Return(exp)
+exp 
+  = Constant(int)
+  | Unary(unary_operator, exp)
+  | Binary(binary_operator, exp, exp)
+unary_operator = Complement | Negate
+binary_operator = Add | Subtract | Multiply | Divide | Remainder
+-}
+
+-- Formal Grammar in EBNF
+{-
+<program> ::= <function>
+<function> ::= "int" <identifier> "(" "void" ")" "{" <statement> "}"
+<statement> ::= "return" <exp> ";"
+<exp> ::= <factor> | <exp> <binop> <exp>
+<factor> ::= <int> | <unop> <factor> | "(" <exp> ")"
+<unop> ::= "-" | "~"
+<binop> ::= "-" | "+" | "*" | "/" | "%"
+<identifier> ::= ? An identifier token ?
+<int> ::= ? A constant token ?
+-}
