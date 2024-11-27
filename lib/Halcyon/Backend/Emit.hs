3abecde27a6a2fa095ebffcba6ecee5391b31fe3
0xpantera <0xpantera@proton.me>
2024-11-27 18:08:01 +0100

    Update assembly emission with binary operators

--- lib/Halcyon/Backend/Emit.hs
+++ lib/Halcyon/Backend/Emit.hs
@@ -2,47 +2,65 @@
 {-# LANGUAGE RecordWildCards #-}
 module Halcyon.Backend.Emit where
 
-import qualified Halcyon.Core.Assembly as Asm
+import Halcyon.Core.Assembly
 import Data.Text ( Text )
 import qualified Data.Text as T
 
 
-showOperand :: Asm.Operand -> Text
-showOperand (Asm.Register Asm.Ax) = "%eax"
-showOperand (Asm.Register Asm.R10) = "%r10d"
-showOperand (Asm.Imm i) = "$" <> T.pack (show i)
-showOperand (Asm.Stack i) = T.pack (show i) <> "(%rbp)"
-showOperand (Asm.Pseudo name) = "%" <> name
+showOperand :: Operand -> Text
+showOperand = \case
+  (Register Ax)  -> "%eax"
+  (Register DX)  -> "%edx"
+  (Register R10) -> "%r10d"
+  (Register R11) -> "%r11d"
+  (Imm i)        -> "$" <> T.pack (show i)
+  (Stack i)      -> T.pack (show i) <> "(%rbp)"
+  (Pseudo name)  -> "%" <> name
 
-showOperator :: Asm.UnaryOp -> Text
-showOperator Asm.Not = "notl"
-showOperator Asm.Neg = "negl"
+showUnaryOp :: UnaryOp -> Text
+showUnaryOp = \case
+  Not -> "notl"
+  Neg -> "negl"
+
+showBinaryOp :: BinaryOp -> Text
+showBinaryOp = \case
+  Add  -> "addl"
+  Sub  -> "subl"
+  Mult -> "imull"
 
 --let show_label name =
 --  match !Settings.platform with OS_X -> "_" ^ name | Linux -> name
 
-emitInstruction :: Asm.Instruction -> Text
-emitInstruction (Asm.Mov src dest) =
-  "\tmovl " <> showOperand src <> ", " <> showOperand dest <> "\n"
-emitInstruction (Asm.AllocateStack i) = 
-  "\tsubq $" <> T.pack (show i) <> ", %rsp\n"
-emitInstruction (Asm.Unary op operand) = 
-  "\t" <> showOperator op <> " " <> showOperand operand <> "\n"
-emitInstruction Asm.Ret = 
-  "\tmovq %rbp, %rsp\n\tpopq %rbp\n\tret\n"
+emitInstruction :: Instruction -> Text
+emitInstruction = \case
+  (Mov src dest) ->
+    "\tmovl " <> showOperand src <> ", " <> showOperand dest <> "\n"
+  (Unary op operand) ->
+    "\t" <> showUnaryOp op <> " " 
+    <> showOperand operand <> "\n"
+  (Binary op src dst) ->
+    "\t" <> showBinaryOp op <> " " <> showOperand src <> ", " <> showOperand dst <> "\n"
+  (Idiv operand) ->
+    "\tidivl " <> showOperand operand <> "\n"
+  Cdq -> 
+    "\tcdq\n"
+  (AllocateStack i) ->
+    "\tsubq $" <> T.pack (show i) <> ", %rsp\n"
+  Ret ->
+    "\tmovq %rbp, %rsp\n\tpopq %rbp\n\tret\n"
 
-emitFunction :: Asm.Function -> Text
-emitFunction (Asm.Function _ instructions) =
+emitFunction :: Function -> Text
+emitFunction (Function _ instructions) =
   let
-    header = 
-      "\t.globl _main\n" <>
-      "_main:\n" <>
-      "\tpushq %rbp\n" <>
-      "\tmovq %rsp, %rbp\n"
+    header 
+      =  "\t.globl _main\n" 
+      <> "_main:\n" 
+      <> "\tpushq %rbp\n" 
+      <> "\tmovq %rsp, %rbp\n"
     instructionsTxt = T.concat $ map emitInstruction instructions
   in
     header <> instructionsTxt 
 
-emitProgram :: Asm.Program -> Text
-emitProgram (Asm.Program func) =
+emitProgram :: Program -> Text
+emitProgram (Program func) =
   emitFunction func
