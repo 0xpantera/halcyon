2ef4ac0b15276e72f22f395aa8eb5956c9449e1f
0xpantera <0xpantera@proton.me>
2024-11-27 18:07:22 +0100

    Update compiler passes to replace pseudo registers and fix up instructions

--- lib/Halcyon/Backend/ReplacePseudos.hs
+++ lib/Halcyon/Backend/ReplacePseudos.hs
@@ -12,7 +12,7 @@ import Control.Monad.State
 import qualified Data.Map.Strict as Map
 import Data.Text ( Text )
 
-import qualified Halcyon.Core.Assembly as Asm
+import Halcyon.Core.Assembly
 
 -- Structure to keep track of what stack slots we've assigned so far
 data ReplacementSt = ReplacementSt
@@ -35,11 +35,11 @@ type MonadReplace m =
   , MonadError ReplacePseudosError m
   )
 
-replaceOperand :: MonadReplace m => Asm.Operand -> m Asm.Operand
-replaceOperand (Asm.Pseudo s) = do
+replaceOperand :: MonadReplace m => Operand -> m Operand
+replaceOperand (Pseudo s) = do
   st <- get
   case Map.lookup s st.offsetMap of
-    Just offset -> pure $ Asm.Stack offset
+    Just offset -> pure $ Stack offset
     Nothing -> do
       let 
         newOffset =  st.currentOffset - 4
@@ -48,51 +48,111 @@ replaceOperand (Asm.Pseudo s) = do
         currentOffset = newOffset,
         offsetMap = newMap
       }
-      pure $ Asm.Stack newOffset
+      pure $ Stack newOffset
 replaceOperand other = pure other
 
-replacePseudoInst :: MonadReplace m => Asm.Instruction -> m Asm.Instruction
+replacePseudoInst :: MonadReplace m => Instruction -> m Instruction
 replacePseudoInst = \case
-  Asm.Mov src dst -> do
+  Mov src dst -> do
     newSrc <- replaceOperand src
     newDst <- replaceOperand dst
-    pure $ Asm.Mov newSrc newDst
-  Asm.Unary op dst -> do
+    pure $ Mov newSrc newDst
+  Unary op dst -> do
     newDst <- replaceOperand dst
-    pure $ Asm.Unary op newDst
-  Asm.Ret -> pure Asm.Ret
-  Asm.AllocateStack _ -> throwError UnexpectedAllocateStack
+    pure $ Unary op newDst
+  Binary op src dst -> do
+    newSrc <- replaceOperand src
+    newDst <- replaceOperand dst
+    pure $ Binary op newSrc newDst
+  Idiv operand -> do
+    newOperand <- replaceOperand operand
+    pure $ Idiv newOperand
+  Cdq -> pure Cdq
+  Ret -> pure Ret
+  AllocateStack _ -> throwError UnexpectedAllocateStack
 
-replacePseudoFunc :: MonadReplace m => Asm.Function -> m Asm.Function
-replacePseudoFunc (Asm.Function name instructions) = do
+replacePseudoFunc :: MonadReplace m => Function -> m Function
+replacePseudoFunc (Function name instructions) = do
   fixedInstructions <- traverse replacePseudoInst instructions
-  pure $ Asm.Function name fixedInstructions
+  pure $ Function name fixedInstructions
 
-replacePseudos :: Asm.Program -> PseudoM (Asm.Program, Int)
-replacePseudos (Asm.Program f) = do
+replacePseudos :: Program -> PseudoM (Program, Int)
+replacePseudos (Program f) = do
   let initState = ReplacementSt 
         { currentOffset = 0
         , offsetMap = Map.empty
         }
-  -- Run the stateful computation and get both result and final state
   (fixedF, finalState) <- runStateT (replacePseudoFunc f) initState
-  -- Return the program and just the offset we need
-  pure (Asm.Program fixedF, currentOffset finalState)
+  pure (Program fixedF, finalState.currentOffset)
 
-fixupInstruction :: Asm.Instruction -> [Asm.Instruction]
-fixupInstruction (Asm.Mov (Asm.Stack src) (Asm.Stack dst)) = 
-  [ (Asm.Mov (Asm.Stack src) (Asm.Register Asm.R10)) 
-  , (Asm.Mov (Asm.Register Asm.R10) (Asm.Stack dst)) ]
-fixupInstruction other = [ other ]
+-- Helper functions to construct common instructions
+movToR10 :: Operand -> Instruction
+movToR10 src = Mov src (Register R10)
 
-fixupFunction :: Asm.Function -> Int -> Asm.Function
-fixupFunction (Asm.Function name instructions) lastStackSlot =
+movFromR10 :: Operand -> Instruction
+movFromR10 dst = Mov (Register R10) dst
+
+movToR11 :: Operand -> Instruction
+movToR11 src = Mov src (Register R11)
+
+movFromR11 :: Operand -> Instruction
+movFromR11 dst = Mov (Register R11) dst
+
+-- Fixes memory-to-memory moves
+fixMemoryMove :: Int -> Int -> [Instruction]
+fixMemoryMove src dst = 
+  [ movToR10 (Stack src)
+  , movFromR10 (Stack dst)
+  ]
+
+-- Fixes immediate operand in idiv
+fixImmediateIdiv :: Int -> [Instruction]
+fixImmediateIdiv n =
+  [ movToR10 (Imm n)
+  , Idiv (Register R10)
+  ]
+
+-- Fixes memory operands in Add/Sub
+fixMemoryBinary :: BinaryOp -> Int -> Int -> [Instruction]
+fixMemoryBinary op src dst =
+  [ movToR10 (Stack src)
+  , Binary op (Register R10) (Stack dst)
+  ]
+
+-- Fixes memory destination in Mult
+fixMemoryMult :: Operand -> Int -> [Instruction]
+fixMemoryMult src dst =
+  [ movToR11 (Stack dst)
+  , Binary Mult src (Register R11)
+  , movFromR11 (Stack dst)
+  ]
+
+fixupInstruction :: Instruction -> [Instruction]
+fixupInstruction = \case 
+  -- Memory-to-memory move
+  Mov (Stack src) (Stack dst) -> 
+    fixMemoryMove src dst
+  -- Immediate operand in idiv  
+  Idiv (Imm n) -> 
+    fixImmediateIdiv n
+  -- Memory operands in Add/Sub
+  Binary op (Stack src) (Stack dst)
+    | op `elem` [Add, Sub] -> 
+      fixMemoryBinary op src dst
+  -- Memory destination in Mult
+  Binary Mult src (Stack dst) ->
+    fixMemoryMult src dst
+  -- No fixup needed
+  other -> [other]
+
+fixupFunction :: Function -> Int -> Function
+fixupFunction (Function name instructions) lastStackSlot =
   let fixedInstructions = 
-        (Asm.AllocateStack lastStackSlot) 
+        (AllocateStack lastStackSlot) 
         : concatMap fixupInstruction instructions
-  in Asm.Function name $ fixedInstructions
+  in Function name $ fixedInstructions
 
 
-fixupProgram :: Asm.Program -> Int -> Asm.Program
-fixupProgram (Asm.Program f) lastStackSlot =
-  Asm.Program (fixupFunction f lastStackSlot) 
+fixupProgram :: Program -> Int -> Program
+fixupProgram (Program f) lastStackSlot =
+  Program (fixupFunction f lastStackSlot) 
